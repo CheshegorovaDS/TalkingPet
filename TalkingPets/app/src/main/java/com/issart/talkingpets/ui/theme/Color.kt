6adcdcad9f82a913584a0724ea027a5162738f46
dcheshegorova <peschink@mail.ru>
2022-01-28 22:26:44 +0600

    add colors for main menu and screen of choosing animal.

--- TalkingPets/app/src/main/java/com/issart/talkingpets/ui/theme/Color.kt
+++ TalkingPets/app/src/main/java/com/issart/talkingpets/ui/theme/Color.kt
@@ -6,3 +6,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Blue = Color(0xFF8AB2ED)
+val Green = Color(0xFF58CCB0)
+val Purple = Color(0xFFBA94E0)
+val Red = Color(0xFFED6D72)
+val Orange = Color(0xFFFFB05B)
+
+val BackgroundColor = Color(0xFFFFF8E7)
+val TextTitleColor = Color(0xFF545047)
+val TextDialogColor = Color(0xFF333333)
+
