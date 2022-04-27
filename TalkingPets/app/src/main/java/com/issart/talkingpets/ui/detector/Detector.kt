cc98ebe494ca810b195472b4643c5218ef0198f5
dcheshegorova <peschink@mail.ru>
2022-04-27 22:17:20 +0600

    Update Detector.kt

--- TalkingPets/app/src/main/java/com/issart/talkingpets/ui/detector/Detector.kt
+++ TalkingPets/app/src/main/java/com/issart/talkingpets/ui/detector/Detector.kt
@@ -1,21 +1,23 @@
 package com.issart.talkingpets.ui.detector
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.issart.talkingpets.R
@@ -60,16 +62,55 @@ fun DetectorPhoto() {
 
 @Composable
 fun DetectorCanvas() {
-    Canvas(modifier = Modifier.fillMaxSize() ) {
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-
-        drawOval(
-            color = Blue,
-            size = Size(canvasWidth * 0.5f, canvasWidth * 0.4f),
-            topLeft = Offset(canvasWidth * 0.25f, canvasHeight / 2)
-        )
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
     }
+
+//    Canvas(modifier = Modifier.fillMaxSize() ) {
+//        val canvasWidth = size.width
+//        val canvasHeight = size.height
+//
+//        drawOval(
+//            color = Blue,
+//            size = Size(canvasWidth * 0.5f, canvasWidth * 0.4f),
+//            topLeft = Offset(canvasWidth * 0.25f, canvasHeight / 2)
+//        )
+//    }
+//    Box(
+//        Modifier
+//            .graphicsLayer(
+//                scaleX = scale,
+//                scaleY = scale,
+//                rotationZ = rotation,
+//                translationX = offset.x,
+//                translationY = offset.y,
+//                shape = RoundedCornerShape(50)
+//            )
+//            .transformable(state = state)
+//            .background(Blue)
+//            .fillMaxSize()
+//    )
+    Image(
+        painter = painterResource(
+            id = R.drawable.ic_rotate_right),
+        contentDescription = "",
+        modifier = Modifier
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                rotationZ = rotation,
+                translationX = offset.x,
+                translationY = offset.y
+            )
+            .transformable(state = state)
+            .background(Color.Transparent)
+            .fillMaxSize()
+    )
 }
 
 @Composable
